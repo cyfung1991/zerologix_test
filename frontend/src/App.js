93e00b5ecec0be722a021dee95557ec689806a4b
FungCheung <cyfung1991@gmail.com>
2022-10-09 10:58:55 +1100

    webinar layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,20 @@
 import './css/app.scss';
-import React, { useState, useEffect } from "react";
+import React from "react";
 import {Heading} from './components/heading.js';
 import {Webinars} from './components/webinars.js';
 import {MeetYourHost} from './components/meet_your_host.js';
 import {RegisterForm} from './components/register_form.js';
+import axios from 'axios';
+
 function App() {
+  // const [data, setData] = React.useState([]);
+  // React.useEffect(() => {
+  //   axios
+  //     .get("http://g1api.finlogix.com/v1/api/post/analysis?per_page=12&page=1")
+  //     .then((res) => {
+  //       setData(res.data)
+  //     })
+  // })
   return (
     <div className="App">
       <Heading />
--- frontend/src/components/webinars.js
+++ frontend/src/components/webinars.js
@@ -1,96 +1,94 @@
 import React, { Component } from 'react';
 
 export class Webinars extends Component {
+    render() {
+        return (
+            <div id="webinars_wrapper">
+                <div className="webinars">
+                    <div className="webinar">
+                        <div className="created_date">
+                            Thursday 9th Oct
+                        </div>
+                        <div className="title">
+                            NFP Preview plus Bank Of Canada
+                        </div>
+                        <div className="content">
+                            A fundamental update getting across the key risk events which create trading opportunities.
+                        </div>
+                        <div className="time">
+                            7pm-8:30pm EST
+                        </div>
+                        <div className="register_wrapper">
+                            <div className="register_now">
+                                Register Now
+                            </div>
+                            <div className="reg_icon"></div>
+                        </div>
+                    </div>
+                    <div className="webinar">
+                        <div className="created_date">
+                            Thursday 9th Oct
+                        </div>
+                        <div className="title">
+                            NFP Preview plus Bank Of Canada
+                        </div>
+                        <div className="content">
+                            A fundamental update getting across the key risk events which create trading opportunities.
+                        </div>
+                        <div className="time">
+                            7pm-8:30pm EST
+                        </div>
+                        <div className="register_wrapper">
+                            <div className="register_now">
+                                Register Now
+                            </div>
+                            <div className="reg_icon"></div>
+                        </div>
+                    </div>
+                    <div className="webinar">
+                        <div className="created_date">
+                            Thursday 9th Oct
+                        </div>
+                        <div className="title">
+                            NFP Preview plus Bank Of Canada
+                        </div>
+                        <div className="content">
+                            A fundamental update getting across the key risk events which create trading opportunities.
+                        </div>
+                        <div className="time">
+                            7pm-8:30pm EST
+                        </div>
+                        <div className="register_wrapper">
+                            <div className="register_now">
+                                Register Now
+                            </div>
+                            <div className="reg_icon"></div>
+                        </div>
+                    </div>
+                    <div className="webinar">
+                        <div className="created_date">
+                            Thursday 9th Oct
+                        </div>
+                        <div className="title">
+                            NFP Preview plus Bank Of Canada
+                        </div>
+                        <div className="content">
+                            A fundamental update getting across the key risk events which create trading opportunities.
+                        </div>
+                        <div className="time">
+                            7pm-8:30pm EST
+                        </div>
+                        <div className="register_wrapper">
+                            <div className="register_now">
+                                Register Now
+                            </div>
+                            <div className="reg_icon"></div>
+                        </div>
+                    </div>
 
-  render() {
-    return(
-      <div id="webinars_wrapper">
-        <div className="webinars">
-
-        <div className="webinar">
-                <div className="created_date">
-                Thursday 9th Oct
-                </div>
-                <div className="title">
-                NFP Preview plus Bank Of Canada
-                </div>
-                <div className="content">
-                A fundamental update getting across the key risk events which create trading opportunities.
-                </div>
-                <div className="time">
-                7pm-8:30pm EST
-                </div>
-                <div className="register_wrapper">
-                    <div className="register_now">
-                        Register Now
-                    </div>
-                    <div className="reg_icon"></div>
                 </div>
             </div>
-            <div className="webinar">
-                <div className="created_date">
-                Thursday 9th Oct
-                </div>
-                <div className="title">
-                NFP Preview plus Bank Of Canada
-                </div>
-                <div className="content">
-                A fundamental update getting across the key risk events which create trading opportunities.
-                </div>
-                <div className="time">
-                7pm-8:30pm EST
-                </div>
-                <div className="register_wrapper">
-                    <div className="register_now">
-                        Register Now
-                    </div>
-                    <div className="reg_icon"></div>
-                </div>
-            </div>
-            <div className="webinar">
-                <div className="created_date">
-                Thursday 9th Oct
-                </div>
-                <div className="title">
-                NFP Preview plus Bank Of Canada
-                </div>
-                <div className="content">
-                A fundamental update getting across the key risk events which create trading opportunities.
-                </div>
-                <div className="time">
-                7pm-8:30pm EST
-                </div>
-                <div className="register_wrapper">
-                    <div className="register_now">
-                        Register Now
-                    </div>
-                    <div className="reg_icon"></div>
-                </div>
-            </div>
-            <div className="webinar">
-                <div className="created_date">
-                Thursday 9th Oct
-                </div>
-                <div className="title">
-                NFP Preview plus Bank Of Canada
-                </div>
-                <div className="content">
-                A fundamental update getting across the key risk events which create trading opportunities.
-                </div>
-                <div className="time">
-                7pm-8:30pm EST
-                </div>
-                <div className="register_wrapper">
-                    <div className="register_now">
-                        Register Now
-                    </div>
-                    <div className="reg_icon"></div>
-                </div>
-            </div>
-
-        </div>
-      </div>
-    )
-  }
+        )
+    }
 
 }
